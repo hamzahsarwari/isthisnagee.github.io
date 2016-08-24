08ce0b37450db92b65ae9e82b1ab3e911bbd82e4
isthisnagee <nagee.elghassein@mail.utoronto.ca>
2016-08-23 19:12:58 -0700

    lmao

--- js/stuff.js
+++ js/stuff.js
@@ -84,6 +84,6 @@ function changeColors(jsonColors){
 		$('.position').css('background-color', highlight);
 		$('.entry').css('border-bottom', '1px dotted ' + dark);
 		$('p .highlight').css('border-bottom', '2px solid ' + highlight);
-		$('pre').css('background-color ', accentDark);
+		$('.jokes').css('background-color ', accentDark);
 	}
 }
